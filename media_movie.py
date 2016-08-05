9bd7af5028af23dbd79901ed9255ea7f1220bd46
Ahmed Elsayed <ahmed.elsayed.mahmoud3@gmail.com>
2016-08-05 22:33:49 +0300

    edited

--- media_movie.py
+++ media_movie.py
@@ -1,7 +1,8 @@
 class Movie:
     """
-    This class  provides a way to store movie related information such as the movie's title, storyline, poster image url and youtube trailer
-    so it can be accessed by fresh_tomatoes.py to construct a website of movie trailers.
+    This class  provides a way to store movie related information such as the
+	movie's title, storyline, poster image url and youtube trailer so it can 
+	be accessed by fresh_tomatoes.py to construct a website of movie trailers.
     """
     
     def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
